0f170430c0683bc2e9875d4158641dc09c94442f
wifi-cam <wifipimpin@gmail.com>
2022-09-14 14:19:13 -0400

    Added JS selectors for the buttons and screen as well as new divs for the screen as well.

---
+++ script.js
@@ -0,0 +1,9 @@
+const numbrBtns = document.querySelectorAll('[data-number');
+const oprtrBtns = document.querySelectorAll('[data-operator]');
+const decimalBtn = document.getElementById('decimalBtn');
+const equalBtn = document.getElementById('equalBtn');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+
+const currentScrn = document.querySelector('.current-input');
+const lastScrn = document.querySelector('.last-input');
