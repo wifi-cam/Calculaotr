3dae65443922d17021e77806492a15f3e6b405a2
wifi-cam <wifipimpin@gmail.com>
2022-09-17 13:07:08 -0400

    Numbers appear on screen now

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const numbrBtns = document.querySelectorAll('[data-number');
-const oprtrBtns = document.querySelectorAll('[data-operator]');
-const decimalBtn = document.getElementById('decimalBtn');
-const equalBtn = document.getElementById('equalBtn');
-const clearBtn = document.getElementById('clear');
-const deleteBtn = document.getElementById('delete');
+let operator = '';
+let currentInput = '';
+let lastInput = '';
 
-const currentScrn = document.querySelector('.current-input');
-const lastScrn = document.querySelector('.last-input');
+document.addEventListener('DOMContentLoaded', function(){
+    let numbrBtns = document.querySelectorAll('.numBtn');
+    const oprtrBtns = document.querySelectorAll('[data-operator]');
+    const decimalBtn = document.getElementById('decimalBtn');
+    const equalBtn = document.getElementById('equalBtn');
+    const clearBtn = document.getElementById('clear');
+    const deleteBtn = document.getElementById('delete');
+
+    let currentScrn = document.querySelector('.current-input');
+    let lastScrn = document.querySelector('.last-input');
+
+    numbrBtns.forEach((number) => number.addEventListener('click', function(e){
+        handleNumber(e.target.textContent)
+        currentScrn.textContent = currentInput;
+    }) )
+})
+
+function handleNumber(num){
+    currentInput += num;
+}
